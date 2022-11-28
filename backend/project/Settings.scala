98cc27586c7e73a96495d0d67c52e7a3fcc66474
Daniel Slapman <danslapman@gmail.com>
2022-11-28 15:53:34 +0100

    Release v3.2.1

--- backend/project/Settings.scala
+++ backend/project/Settings.scala
@@ -26,7 +26,7 @@ object Settings {
 
   val common = Seq(
     organization := "ru.tinkoff",
-    version := "3.1.0",
+    version := "3.2.1",
     scalaVersion := "2.13.10",
     Compile / packageDoc / publishArtifact := false,
     Compile / packageSrc / publishArtifact := false,
